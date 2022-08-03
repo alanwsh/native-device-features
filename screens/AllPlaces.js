dca6a7278a07a02c5724f1b02305b32248d6fa8d
alanwsh <72345813+alanwsh@users.noreply.github.com>
2022-08-03 22:09:28 +0800

    fetch places from db

--- screens/AllPlaces.js
+++ screens/AllPlaces.js
@@ -1,16 +1,21 @@
 import { useIsFocused } from "@react-navigation/native";
 import { useEffect, useState } from "react";
 import PlacesList from "../components/Places/PlacesList";
+import { fetchPlaces } from "../utils/database";
 
 function AllPlaces({route}){
     const [places, setPlaces] = useState([]);
     const isFocused = useIsFocused();
 
     useEffect(() => {
-        if(isFocused && route.params){
-            setPlaces(curr => [...curr, route.params.place]);
+        async function loadPlaces(){
+            const places = await fetchPlaces();
+            setPlaces(places);
         }
-    },[isFocused, route]);
+        if(isFocused){
+            loadPlaces();
+        }
+    },[isFocused]);
     
     return <PlacesList places={places}/>;
 }
